97b437e68fcedbfa3d8daa71d7021f506f336c41
Richard Hull <rm_hull@yahoo.co.uk>
2021-09-12 19:43:49 +0100

    File info

--- app.py
+++ app.py
@@ -1,60 +1,80 @@
-from typing import List
-from flask import Flask, request, render_template, redirect
 import os
+import time
+from typing import Dict, List
+from flask import Flask, request, render_template, redirect
 from pathlib import Path
 
 app = Flask(__name__)
 
 
-def get_videos(path: str) -> List[str]:
-    return sorted(
-        file
-        for file in os.listdir(os.path.join("static/data", path))
-        if file.endswith(".mp4") or os.path.isdir(os.path.join("static/data", path, file))
+def get_files(path: str) -> List[Dict[str, str]]:
+    return [
+        dict(filename=file, stat=os.stat(os.path.join("static/data", path, file)))
+        for file in sorted(os.listdir(os.path.join("static/data", path)))
+        if file.endswith(".mp4")
+        or os.path.isdir(os.path.join("static/data", path, file))
+    ]
+
+
+def index_of(arr: List[Dict[str, str]], filename) -> int:
+    return [item["filename"] for item in arr].index(filename)
+
+
+@app.route("/<path:parent>/<string:file>.mp4")
+def file(parent: str, file: str):
+    files = get_files(parent)
+    index = index_of(files, f"{file}.mp4")
+    count = len(files)
+    prev = files[index - 1]["filename"] if index > 0 else None
+    next = files[index + 1]["filename"] if index + 1 < count else None
+    time = f"{file[-6:-4]}:{file[-4:-2]}:{file[-2:]}"
+    return render_template(
+        "file.html",
+        parent=parent,
+        index=index + 1,
+        count=count,
+        file=f"{file}.mp4",
+        time=time,
+        next=next,
+        prev=prev,
     )
 
 
-@app.route('/<path:parent>/<string:file>.mp4')
-def file(parent: str, file: str):
-    videos = get_videos(parent)
-    index = videos.index(f"{file}.mp4")
-    count = len(videos)
-    prev = videos[index - 1] if index > 0 else None
-    next = videos[index + 1] if index + 1 < count else None
-    time = f"{file[-6:-4]}:{file[-4:-2]}:{file[-2:]}"
-    return render_template('file.html',
-                           parent=parent,
-                           index=index + 1,
-                           count=count,
-                           file=f"{file}.mp4",
-                           time=time,
-                           next=next,
-                           prev=prev)
-
-
-@app.route('/')
-@app.route('/<path:path>/')
-def folder(path: str = ''):
+@app.route("/")
+@app.route("/<path:path>/")
+def folder(path: str = ""):
     parent = Path(path).parent
-    return render_template('folder.html',
-                           path=path,
-                           parent=None if str(parent) == '.' else parent,
-                           videos=get_videos(path))
+    return render_template(
+        "folder.html",
+        path=path,
+        parent=None if str(parent) == "." else parent,
+        videos=get_files(path),
+    )
 
 
-@app.route('/delete-file', methods=['POST'])
+@app.route("/delete-file", methods=["POST"])
 def delete_file():
-    file = request.form['file']
+    file = request.form["file"]
     path = os.path.join("static/data", file)
     basename = os.path.basename(path)
-    
+
     parent = Path(file).parent
-    videos = get_videos(parent)
-    index = videos.index(basename)
-    count = len(videos)
-    next = videos[index + 1] if index + 1 < count else None
-    
+    files = get_files(parent)
+    index = index_of(files, basename)
+    count = len(files)
+    if index + 1 < count:
+        next = files[index + 1]["filename"]
+    elif index > 0:
+        next = files[index - 1]["filename"]
+    else:
+        next = None
+
     print(f"Deleting file: {path}")
     os.rename(path, f"static/data/trash/{basename}")
-    
-    return redirect(f'/{parent}/{next}' if next else f'/{parent}', code=302)
+
+    return redirect(f"/{parent}/{next}" if next else f"/{parent}", code=302)
+
+
+@app.template_filter("ctime")
+def timectime(s):
+    return time.ctime(s)  # datetime.datetime.fromtimestamp(s)
